af372dcd5555b8b205f10a481067c5534cf414e9
nuno-venturinha <nuno.venturinha.f@tecnico.ulisboa.pt>
2017-11-13 01:15:02 +0000

    Updated print

--- project_2/route_finder/route_finder.c
+++ project_2/route_finder/route_finder.c
@@ -20,192 +20,207 @@ typedef struct Graph_t
 
 typedef struct StackNode_t
 {
-	int vertex;
-	struct StackNode_t *next;
+    int vertex;
+    struct StackNode_t *next;
 } StackNode;
 
 StackNode *create_stack_node(int vertex)
 {
-	StackNode *node = (StackNode *) malloc(sizeof(StackNode));
+    StackNode *node = (StackNode *) malloc(sizeof(StackNode));
 
-	node->vertex = vertex;
-	node->next = NULL;
+    node->vertex = vertex;
+    node->next = NULL;
 
-	return node;
+    return node;
 }
 
 int is_empty(StackNode *root)
 {
-	return !root;
+    return !root;
 }
 
 void push(StackNode **root, int vertex)
 {
-	StackNode *node = create_stack_node(vertex);
+    StackNode *node = create_stack_node(vertex);
 
-	node->next = *root;
-	*root = node;
+    node->next = *root;
+    *root = node;
 }
 
 int pop(StackNode **root)
 {
-	if(is_empty(*root))
-	{
-		return -1;
-	}
+    if(is_empty(*root))
+    {
+        return -1;
+    }
 
-	StackNode *temp = *root;
-	*root = (*root)->next;
+    StackNode *temp = *root;
+    *root = (*root)->next;
 
-	int popped = temp->vertex;
+    int popped = temp->vertex;
 
-	free(temp);
+    free(temp);
 
-	return popped;
+    return popped;
 }
 
 
 void shortest_path(Graph *graph, int src)
 {
 
-	int *visited;
-	int vertex;
-	int *routes;
+    int *visited;
+    int vertex;
+    int *routes;
 
-	visited = (int *) calloc((graph->V), sizeof(int));
-	routes = (int *) calloc((graph->V), sizeof(int));
+    visited = (int *) calloc((graph->V), sizeof(int));
+    routes = (int *) calloc((graph->V), sizeof(int));
+    for(int i = 1; i < graph->V; i++)
+    {
+        if(graph->list[i] != NULL)
+        {
+            routes[i] = INT_MAX;
+        }
+    }
 
-	StackNode *customer = NULL;
-	StackNode *peer = NULL;
-	StackNode *provider = NULL;
+    StackNode *customer = NULL;
+    StackNode *peer = NULL;
+    StackNode *provider = NULL;
 
     AdjListNode *curr = graph->list[src];
     while(curr != NULL)
     {
-    	switch(curr->type)
-    	{
-    		case 1:
-    			push(&customer, curr->dest);
-    			break;
-    		case 2:
-    			push(&peer, curr->dest);
-    			break;
-    		case 3:
-    			push(&provider, curr->dest);
-    			break;
-    		default:
-    			break;
-    	}
+        switch(curr->type)
+        {
+            case 1:
+                push(&customer, curr->dest);
+                break;
+            case 2:
+                push(&peer, curr->dest);
+                break;
+            case 3:
+                push(&provider, curr->dest);
+                break;
+            default:
+                break;
+        }
         curr = curr->next;
     }
 
     LOOP:
     while(provider != NULL)
     {
-    	vertex = pop(&provider);
-    	if(vertex != -1 && visited[vertex] == 0)
-    	{
-    		visited[vertex] = 1;
-    		routes[vertex] = 3;
-    		curr = graph->list[vertex];
-    		while(curr != NULL)
-    		{
-    			if(visited[curr->dest] == 0)
-    			{
-			    	switch(curr->type)
-			    	{
-			    		case 1:
-			    			push(&customer, curr->dest);
-			    			break;
-			    		case 2:
-			    			push(&peer, curr->dest);
-			    			break;
-			    		case 3:
-			    			push(&provider, curr->dest);
-			    			break;
-			    		default:
-			    			break;
-			    	}
-			    }
-		        curr = curr->next;
-    		}
-    	}
-    	goto LOOP;
+        vertex = pop(&provider);
+        if(vertex != -1 && visited[vertex] == 0)
+        {
+            visited[vertex] = 1;
+            routes[vertex] = 3;
+            curr = graph->list[vertex];
+            while(curr != NULL)
+            {
+                if(visited[curr->dest] == 0)
+                {
+                    switch(curr->type)
+                    {
+                        case 1:
+                            push(&customer, curr->dest);
+                            break;
+                        case 2:
+                            push(&peer, curr->dest);
+                            break;
+                        case 3:
+                            push(&provider, curr->dest);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                curr = curr->next;
+            }
+        }
+        goto LOOP;
     }
     while(peer != NULL)
     {
-    	vertex = pop(&peer);
-    	if(vertex != -1 && visited[vertex] == 0)
-    	{
-    		visited[vertex] = 1;
-    		routes[vertex] = 2;
-    		curr = graph->list[vertex];
-    		while(curr != NULL)
-    		{
-    			if(visited[curr->dest] == 0)
-    			{
-			    	switch(curr->type)
-			    	{
-			    		case 1:
-			    			push(&customer, curr->dest);
-			    			break;
-			    		default:
-			    			break;
-			    	}
-			    }
-		        curr = curr->next;
-    		}
-    	}
-    	goto LOOP;
+        vertex = pop(&peer);
+        if(vertex != -1 && visited[vertex] == 0)
+        {
+            visited[vertex] = 1;
+            routes[vertex] = 2;
+            curr = graph->list[vertex];
+            while(curr != NULL)
+            {
+                if(visited[curr->dest] == 0)
+                {
+                    switch(curr->type)
+                    {
+                        case 1:
+                            push(&customer, curr->dest);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                curr = curr->next;
+            }
+        }
+        goto LOOP;
     }
     while(customer != NULL)
     {
-    	vertex = pop(&customer);
-    	if(vertex != -1 && visited[vertex] == 0)
-    	{
-    		visited[vertex] = 1;
-    		routes[vertex] = 1;
-    		curr = graph->list[vertex];
-    		while(curr != NULL)
-    		{
-    			if(visited[curr->dest] == 0)
-    			{
-			    	switch(curr->type)
-			    	{
-			    		case 1:
-			    			push(&customer, curr->dest);
-			    			break;
-			    		default:
-			    			break;
-			    	}
-			    }
-		        curr = curr->next;
-    		}
-    	}
-		goto LOOP;
+        vertex = pop(&customer);
+        if(vertex != -1 && visited[vertex] == 0)
+        {
+            visited[vertex] = 1;
+            routes[vertex] = 1;
+            curr = graph->list[vertex];
+            while(curr != NULL)
+            {
+                if(visited[curr->dest] == 0)
+                {
+                    switch(curr->type)
+                    {
+                        case 1:
+                            push(&customer, curr->dest);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                curr = curr->next;
+            }
+        }
+        goto LOOP;
     }
+
 
     for(int i = 1;i < graph->V; i++)
     {
-    	if(i != src)
-    	{
-	    	switch(routes[i])
-	    	{
-	    		case 1:
-	    			fprintf(stdout, "%d uses provider route to %d\n", i, src);
-	    			break;
-	    		case 2:
-	    			fprintf(stdout, "%d uses peer route to %d\n", i, src);
-	    			break;
-	    		case 3:
-	    			fprintf(stdout, "%d uses customer route to %d\n", i, src);
-	    			break;
-	    		default:
-	    			fprintf(stdout, "%d has no route to %d\n", i, src);
-	    			break;
-	    	}
-	    }
+        if(i != src)
+        {
+            /*
+            switch(routes[i])
+            {
+                case 1:
+                    fprintf(stdout, "%d uses provider route to %d\n", i, src);
+                    break;
+                case 2:
+                    fprintf(stdout, "%d uses peer route to %d\n", i, src);
+                    break;
+                case 3:
+                    fprintf(stdout, "%d uses customer route to %d\n", i, src);
+                    break;
+                case INT_MAX:
+                    fprintf(stdout, "%d has no route to %d\n", i, src);
+                    break;
+                default:
+                    break;
+            }
+            */
+            if(routes[i] != 0)
+                fprintf(stdout, "%d %d %d\n", i, routes[i], src);
+        }
     }
+
     free(visited);
     free(routes);
 }
@@ -254,11 +269,11 @@ Graph *create_graph_from_file(char *filename)
     Graph *graph = NULL;
     FILE *file = fopen(filename, "r");
 
-    int highest_id	= 0;
-    int node_id		= 0;
-    int src			= 0;
-    int dest		= 0;
-    int type		= 0;
+    int highest_id  = 0;
+    int node_id     = 0;
+    int src         = 0;
+    int dest        = 0;
+    int type        = 0;
 
     while(fscanf(file, "%d", &node_id) == 1)
     {
@@ -308,10 +323,14 @@ int main(int argc, char **argv)
 
     graph = create_graph_from_file(argv[1]);
 
-    for(int i = 1; i < graph->V; i++)
+    /*
+    for(int i = 1; i < 200; i++)
     {
-    	shortest_path(graph, i);
+        shortest_path(graph, i);
     }
+    */
+
+    shortest_path(graph, 1234);
 }
 
 
